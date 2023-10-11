257babd67ed60b534e1b105a9135835da9a63700
Giovanni Karra <giovanni.karra@student.uclouvain.be>
2023-10-11 18:12:54 +0200

    Weird first version of the display

--- Core/display.py
+++ Core/display.py
@@ -1,12 +1,30 @@
+import os
+
 class display:
-    
-    width : int = None
-    height : int = None
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self._width : int = width
+        self._height : int = height
+
+        self._screen : list[list[str]] = [["."] * width] * height
+
+
+    def update_display(self):
+        self.clear()
+        rows = ["".join(r) for r in self._screen]
+        print("\n".join(rows))
+
+
+    def set_pixels(self, value : str, x : int, y : int):
+        self._screen[y][x] = value
+
+
+    def clear(self):
+        os.system("cls")
 
 
 if __name__ == "__main__":
-    pass
+    my_display = display(20, 10)
+    my_display.set_pixels("#", 5, 5)
+    my_display.update_display()
+    input()
