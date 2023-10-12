6e7397aba924c3a7abd135669209461b2591fbbe
Giovanni Karra <giovanni.karra@student.uclouvain.be>
2023-10-12 23:14:06 +0200

    display -> class methods

--- Core/display.py
+++ Core/display.py
@@ -2,29 +2,38 @@ import os
 
 class Display:
 
-    def __init__(self, width : int, height : int):
-        self._width : int = width
-        self._height : int = height
-
-        self._screen : list[list[str]] = [["." for _ in range(width)] for _ in range(height)]
+    _width : int
+    _height : int
+    _screen : list[list[str]]
 
 
-    def update_display(self) -> None:
-        self.clear()
-        rows = ["".join(r) for r in self._screen]
+    @classmethod
+    def initialize(cls, width : int, height : int):
+        cls._width : int = width
+        cls._height : int = height
+
+        cls._screen : list[list[str]] = [["." for _ in range(width)] for _ in range(height)]
+
+
+    @classmethod
+    def update_display(cls) -> None:
+        cls.clear()
+        rows = ["".join(r) for r in cls._screen]
         print("\n".join(rows))
 
 
-    def set_pixels(self, value : str, x : int, y : int) -> None:
-        self._screen[y][x] = value
+    @classmethod
+    def set_pixels(cls, value : str, x : int, y : int) -> None:
+        cls._screen[y][x] = value
 
 
-    def clear(self) -> None:
+    @classmethod
+    def clear(cls) -> None:
         os.system("cls")
 
 
 if __name__ == "__main__":
-    my_display = Display(20, 10)
-    my_display.set_pixels("#", 5, 5)
-    my_display.update_display()
+    Display.initialize(50, 10)
+    Display.set_pixels("#", 5, 5)
+    Display.update_display()
     input()
--- Core/gameobject.py
+++ Core/gameobject.py
@@ -1,2 +1,4 @@
 class GameObject:
-    pass
+    
+    def __init__(self):
+        pass
