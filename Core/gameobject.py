f05c8fe787f888e5df615e72c78760abb89d7015
Giovanni Karra <giovanni.karra@student.uclouvain.be>
2023-10-16 20:22:06 +0200

    Sprite foundations

--- Core/gameobject.py
+++ Core/gameobject.py
@@ -1,24 +1,35 @@
+from renderer import Sprite
+
 class GameObject:
     
     def __init__(self):
         self._x : int
         self._y : int
+        self._sprite : Sprite = None
 
         GameObjectManager.send_object(self)
 
 
-    def set_position(self, x : int, y : int):
+    def update(self) -> None:
+        pass
+
+
+    def set_position(self, x : int, y : int) -> None:
         self.x = x
         self.y = y
 
 
-    def get_position(self):
+    def get_position(self) -> (int, int):
         return self.x, self.y
 
 
-    def destroy(self):
+    def destroy(self) -> None:
         GameObjectManager.remove_object(self)
         del self
+
+    
+    def set_sprite(self, sprite : Sprite) -> None:
+        self._sprite = sprite
 
 
 
--- Core/renderer.py
+++ Core/renderer.py
@@ -1,2 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class Sprite:
+    sprite : list[list[str]]
+    layer : int
+
+
 class Renderer:
-    pass
+
+    @staticmethod
+    def create_sprite_from_string(string : str, layer : int = 0) -> Sprite:
+        lines : list[str] = string.split("\n")
+        sprite : list[list[str]] = [lines[i].split for i in range(len(lines))]
+
+        return Sprite(sprite, layer)
