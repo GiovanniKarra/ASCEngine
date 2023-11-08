55d3d20fceae215ac123f7e991b3acdbf8e02342
Giovanni Karra <karragiovanni@gmail.com>
2023-11-08 10:07:15 +0100

    package setup system

---
+++ setup_packages.py
@@ -0,0 +1,12 @@
+from os import system, remove
+from sys import argv
+
+system("pip install pynput")
+system("pip install consoledraw")
+system("pip install termcolor")
+
+system("pip install -e .")
+
+input("DONE\nThis file is going to be deleted\nPress ENTER to continue")
+
+remove(argv[0])
